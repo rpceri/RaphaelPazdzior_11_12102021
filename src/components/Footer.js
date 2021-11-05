b7b4bccc3f0fc1df656173a65e6edce8737ef4f9
Raphael Pazdzior <rpazdzior@ceri-informatique.fr>
2021-11-05 11:56:39 +0100

    fix: modif css footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,9 @@ export default class Footer extends Component {
     render() {
         return (
             <footer>
+                <div className='logo'>
+                    <img src={`${process.env.PUBLIC_URL}/img/logo_kasa_blanc.png`} alt="Kasa" />
+                </div>
                 <p>2020 Kasa. All rights reserved</p>
             </footer>        
         )
